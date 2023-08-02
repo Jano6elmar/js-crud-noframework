b8e43d1c747b57b0648434bdcf908fbaa47bab89
Jan Belmar <jano6belmar@gmail.com>
2023-08-02 05:43:44 -0400

    "traer data"

--- src/users/store/user-store.js
+++ src/users/store/user-store.js
@@ -1,6 +1,47 @@
+import { loadUsersByPage } from "../use-cases/load-users-by-page"
 
 
 const state = {
     currentPage: 0,
     users: [],
+}
+
+const loadNextPage = async() => {
+
+    const users = await loadUsersByPage( state.currentPage + 1)
+    if ( users.length === 0 ) return;
+    state.currentPage += 1;
+    state.users = users;
+
+};
+
+const loadPreviousPage = async() => {
+    throw new Error ('No implemented yet')
+   
+}
+
+
+const onUserChanged = (updatedUser) => {}
+
+//TODO
+const reloadPage = async() => {
+   throw new Error ('No implemented yet')
+}
+
+export default {
+    loadNextPage,
+    loadPreviousPage,
+    onUserChanged,
+    reloadPage,
+
+
+    /**
+     * @returns {User[]}
+     */
+    getUsers: () => [...state.users],
+
+    /**
+     * @returns {Number}
+     */
+    getCurrentPage: () => state.currentPage,
 }
